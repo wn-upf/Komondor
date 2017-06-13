b5f4cfce5c6df940a6e9ec952afbbef638596de5
sergiobarra <sergio.barrachina@upf.edu>
2017-06-13 15:10:04 +0200

    komondor input generator II
    This closes #46

--- apps/komondor_input_generator/src/nodes_deterministic_generator.java
+++ apps/komondor_input_generator/src/nodes_deterministic_generator.java
@@ -3,16 +3,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-import java.util.Arrays;
-import javax.swing.JList;
 
 /*
  * Program for generating deterministic node inputs in csv files
@@ -67,10 +63,10 @@ public class nodes_deterministic_generator {
 
     static int letter_counter;
 
-    private static final char DEFAULT_SEPARATOR = ';';
+    private static final char DEFAULT_SEPARATOR = ',';
 
     static String csv_output_directory = "output_csv";
-    static String csv_filepath_root = "nodes_input";
+    static String csv_filepath_root = "input_nodes";
 
     @SuppressWarnings("empty-statement")
     public static void input_attributes(String input_path) throws FileNotFoundException, IOException {
@@ -268,7 +264,7 @@ public class nodes_deterministic_generator {
                 node_type = 0;
                 wlan_code = DICTIONARY[letter_counter];
                 node_code = "AP_";
-                x += distance_between_aps;
+                x = x + distance_between_aps;
                 y = 0;
 
                 letter_counter++;
@@ -311,7 +307,11 @@ public class nodes_deterministic_generator {
 
             System.out.println(line);
 
-            out.println(line);
+             out.print(line);
+             
+            if(line_ix < num_lines - 1){
+               out.print("\n");
+            }
 
             // file_in_text = file_in_text.concat(line + "\n");
         }
